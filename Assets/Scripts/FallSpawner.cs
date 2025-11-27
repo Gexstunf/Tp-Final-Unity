73ce4d5aa618d4c537e1ab777648affe1ac2f7e7
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-11-27 18:39:17 -0300

    no se

---
+++ Assets/Scripts/FallSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallSpawner : MonoBehaviour
+{
+    public float fallY = -15f;          // Altura donde se considera que cayó
+    public Vector3 respawnPoint;        // Lugar donde respawnea
+
+    void Update()
+    {
+        if (transform.position.y < fallY)
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnPoint;
+    }
+}
