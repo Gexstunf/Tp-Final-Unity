499e1732e6a68ed165eb7cff2d8bb4eed623607a
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-11-30 16:08:04 -0300

    coins

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.instance.AddCoin();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject coinPrefab;
+
+    [SerializeField] private float spawnChance = 0.6f;
+    [SerializeField] private int totalCoins = 10;
+    [SerializeField] private int collectedCoins;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        SpawnCoins();
+    }
+
+    void SpawnCoins()
+    {
+        // Encuentra todos los spawn points
+        GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("CoinSpawn");
+
+        foreach (GameObject spawn in spawnPoints)
+        {
+            if (Random.value <= spawnChance)
+            {
+                Instantiate(coinPrefab, spawn.transform.position, Quaternion.identity);
+                totalCoins++;
+            }
+        }
+
+        Debug.Log("Monedas generadas: " + totalCoins);
+    }
+
+    public void AddCoin()
+    {
+        collectedCoins++;
+
+        if (collectedCoins >= totalCoins)
+        {
+            GameCompleted();
+        }
+    }
+
+    void GameCompleted()
+    {
+        Debug.Log("¡Juego completado! Todas las monedas generadas fueron recogidas.");
+    }
+}
